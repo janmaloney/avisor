d9ff13b04bb100e1e0597781c0fd60806ff584fc
Janice Maloney <janmaloney@gmail.com>
2014-04-09 22:52:40 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Avisor::Application.config.secret_key_base = '42f8195e41e2e59462853a6ad8ea76bc7388b673255db8a6e3e9d8f1b4ba142e2599c3106f0234e051506225fefda5d616852796912278dc57aee03fbb0354b5'
