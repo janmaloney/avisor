a16e6162153859c0030d2ac71bb6e7ffca403cd1
Janice Maloney <janmaloney@gmail.com>
2014-04-17 18:36:16 +0000

    add changes for devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Avisor::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'avisor-janice-maloney.herokuapp.com'}
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
